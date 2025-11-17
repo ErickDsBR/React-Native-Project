bb4b75cdf1e1e64b7d601613246a9be6c80b798f
ErickDsBR <erick.brasil2102@gmail.com>
2025-11-17 16:45:42 -0300

    modificaçoes

--- App.js
+++ App.js
@@ -3,7 +3,7 @@
 //* criar um pasta separar o codigo de funções e componentes um arquivo que guarda a lista como um bd e um para as funçoes e paginas do app com coisas relacionadas a interface e coisas a mais de um mercado!! 
 
 import React, { useState } from "react";
-import { TextInput, Modal, Button, StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { TextInput, Modal, Button, StyleSheet, Text, View, TouchableOpacity, ScrollView } from "react-native";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 //!!.............................................
 
@@ -15,15 +15,32 @@ export default function App() {
       id: 1,
       nome: "maça",
       valor: 2.50,
+      categoria: "frutas",
     },
     {
       id: 2,
+      categoria: "frutas",
+      img: "🍌",
       nome: "banana",
       valor: 50,
     },
+    {
+      id: 3,
+      nome: "Café",
+      valor: 150.75,
+      categoria: "Cafe em po",
+    }
   ]);
-  
+  const [cart, setcart] = useState([
+    {
+
+    }
+
+
+  ]);
+
   const [modalVisible, setModalVisible] = React.useState(false);
+  const [carrinho, setCarrinho] = useState(false);
 
   const adicionarProduto = () => {
     const novoProduto = {
@@ -39,19 +56,35 @@ export default function App() {
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
         <View style={styles.header}>
-          <Text style={styles.headerName}>Mercadinho Do Seu Zé</Text>
+          <TextInput style={styles.headersearch} placeholder="Pesquisar Produto" />
         </View>
-        <View style={styles.produtos}>
-          {produtos.map((produto) => (
-            <View key={produto.id} style={styles.produto}>
-              <Text>{produto.nome}</Text>
-              <Text>R$ {produto.valor.toFixed(2)}</Text>
-            </View>
-          ))}
+        <ScrollView style={styles.scrollView}>
+          <View style={styles.produtos}>
+            {produtos.map((produto) => (
+              <View key={produto.id} style={styles.produto}>
+                <View style={styles.imgContainer}>
+                  <Text style={styles.emoji} >{produto.img}</Text>
+                </View>
+                <Text>{produto.nome}</Text>
+                <Text>R$ {produto.valor.toFixed(2)}</Text>
+                <TouchableOpacity style={styles.addcart} onPress={() => { }}>
+                  <Text>Adicionar ao Carrinho</Text>
+                </TouchableOpacity>
+              </View>
+
+            ))}
+          </View>
+        </ScrollView>
+        <View style={styles.navgation}>
+          <TouchableOpacity style={styles.navgation_button} title="Adicionar Produto" onPress={() => setModalVisible(true)}>
+            <Text>🏬</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.navgation_button} title="Adicionar Produto" onPress={() => setCarrinho(true)}>
+            <Text>🛒</Text>
+          </TouchableOpacity>
         </View>
-        <Button title="Adicionar Produto" onPress={() => setModalVisible(true)}/>
       </SafeAreaView>
-      <Modal 
+      <Modal
         justifyContent="center"
         animationType="slide"
         transparent={true}
@@ -65,7 +98,7 @@ export default function App() {
               style={styles.input}
               onChangeText={setName}
               value={Name}
-              placeholder="Nome do Produto" 
+              placeholder="Nome do Produto"
             />
             <TextInput
               style={styles.input}
@@ -84,41 +117,62 @@ export default function App() {
           </View>
         </SafeAreaView>
       </Modal>
+      <SafeAreaView style={styles.modalcart}>
+        <Modal
+          justifyContent="center"
+          animationType="slide"
+          visible={carrinho}
+          onRequestClose={() => setCarrinho(false)}
+        >
+          <SafeAreaView style={styles.modalContainer}>
+            <View style={styles.cartView}>
+              <Text style={styles.cartheader} >Carrinho</Text>
+              <TouchableOpacity style={styles.botao} onPress={() => setCarrinho(false)}>
+                <Text>Fechar</Text>
+              </TouchableOpacity>
+            </View>
+          </SafeAreaView>
+        </Modal>
+      </SafeAreaView>
+
     </SafeAreaProvider >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#9d9d9d",
     alignItems: "center",
-    backgroundColor: "#000",
     flex: 1,
   },
   header: {
-    alignItems: "center",
-    justifyContent: "center",
-    alignContent: "center",
-    padding: "10px",
-    borderRadius: "15px",
-    elevation: 10,
-    backgroundColor: "#9d9d9d",
-    width: "85%",
+    width: "100%",
     height: "50px",
   },
-  headerName: {
+  headersearch: {
+    backgroundColor: "#9d9d9d",
+    borderRadius: 15,
+    padding: 10,
+    textAlign: "center",
     fontSize: 15,
     fontWeight: "bold",
+    width: "100%",
+    height: "50px",
   },
   produtos: {
+    marginTop: 10,
+    elevation: 10,
+    padding: "10px",
     backgroundColor: "#9d9d9d",
     justifyContent: "center",
     flexWrap: "wrap",
     flexDirection: "row",
-    paddingTop: "10px",
     width: "100%",
     gap: '1%',
   },
   produto: {
+    elevation: 10,
+    marginTop: 5,
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#fff",
@@ -126,6 +180,20 @@ const styles = StyleSheet.create({
     height: 300,
     borderRadius: 15,
     elevation: 5,
+  },
+  imgContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: 150,
+  },
+  emoji: {
+    fontSize: 80,
+    marginBottom: 100,
+  },
+  scrollView: {
+    width: "100%",
+    marginBottom: 10,
   },
   modalView: {
     backgroundColor: "#9d9d9d",
@@ -174,6 +242,28 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginTop: 10,
 
-  }
+  },
+  navgation: {
+    backgroundColor: "#9d9d9d",
+    padding: 10,
+    borderRadius: 15,
+    elevation: 10,
+    justifyContent: "space-around",
+    flexDirection: "row",
+    width: "100%",
+  },
+  navgation_button: {
+    width: 120,
+    borderRadius: 15,
+    elevation: 10,
+    padding: 10,
+    marginBottom: 10,
+    justifyContent: "space-evenly",
+    backgroundColor: "#1fdfb6ff",
+    alignItems: "center",
+  },
+
+
+
 
 });
